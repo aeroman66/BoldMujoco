fb194a6c8e0079d0405d4e8fe110df7ff83312ef
Heng Ye <207193853@qq.com>
2024-08-19 00:25:48 +0800

    test gitignore.
    It's the first time that I used a gitignore file.

--- whatever/import_test/test_module.py
+++ whatever/import_test/test_module.py
@@ -1,8 +1,8 @@
-def haha():
-    print("haha") # 但是问题是这个脚本并没有被执行
+def laugh():
+    print("laugh") # 但是问题是这个脚本并没有被执行
 
 print(__name__)
 
 # 这个的意思就是，只有在当前文件中运行时，才会执行下面的语句，在其他文件中导入时，不会执行下面的语句
 if __name__ == '__main__':
-    haha()
+    laugh()
